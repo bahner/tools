f1ea3001983a9ab29e793ed4ebe160aee90b614f
Lars Bahner <lars.bahner@gmail.com>
2018-02-15 23:51:01 +0100

    sort and allow for reading any region

--- ip-ranges.py
+++ ip-ranges.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
-""" Takes one positional parameter - an AWS region name
-and prints the ipranges in this region.
-"""
-import requests
-import sys
+""" Takes one positional parameter an AWS region name
 
-region = sys.argv[1]
+Prints the ipranges in this region.
+"""
+
+import sys
+import requests
+
+try:
+    region = sys.argv[1]
+except IndexError:
+    region = 'any'
 
 ranges = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()
+prefixes = set()
 
 for prefix in ranges['prefixes']:
-    if prefix['region'] == region:
-        print(prefix['ip_prefix'])
+    if prefix['region'] == region or region == 'any':
+        prefixes.add(prefix['ip_prefix'])
+
+for prefix in sorted(prefixes):
+    print(prefix)
