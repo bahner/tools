af40b30d5008f5c8fd4f732fe314d12e8df12b9f
Lars Bahner <lars.bahner@gmail.com>
2020-09-21 18:38:03 +0200

    Sort output. Might serve someone.

--- ip-ranges.py
+++ ip-ranges.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
-""" Takes one positional parameter an AWS region name
-
-Prints the ipranges in this region.
+""" Takes one positional parameter - an AWS region name
+and prints the ipranges in this region.
 """
-
-import sys
 import requests
+import sys
+import ipaddress
+
+PREFIXES=set()
 
 try:
     region = sys.argv[1]
 except IndexError:
-    region = 'any'
+    # If no parameter is given print the all
+    region = 'all'
 
 ranges = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()
-prefixes = set()
 
 for prefix in ranges['prefixes']:
-    if prefix['region'] == region or region == 'any':
-        prefixes.add(prefix['ip_prefix'])
+    if prefix['region'] == region or region == 'all':
+        PREFIXES.add(prefix['ip_prefix'])
 
-for prefix in sorted(prefixes):
+for prefix in sorted(PREFIXES, key = ipaddress.IPv4Network):
     print(prefix)
